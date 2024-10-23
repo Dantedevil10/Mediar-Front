d68dfab8951aeab22159271564637c47cd6ed8f7
AndreAllyson <allysonrodriguesx@gmail.com>
2024-10-22 21:00:10 -0300

    Semi Finalização página andamento

--- src/app/components/teladeandamento/teladeandamento.component.ts
+++ src/app/components/teladeandamento/teladeandamento.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-teladeandamento',
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrl: './teladeandamento.component.css'
 })
 export class TeladeandamentoComponent {
+  
+  @ViewChild('teste') teste!: ElementRef;
 
+  aberto:boolean=true;
+  analise:boolean=false;
+  fechado:boolean=false;
+
+  constructor(private renderer: Renderer2) {}
+
+  ngAfterViewInit() {
+    if (this.aberto && !this.fechado && !this.analise) {
+      this.renderer.setStyle(this.teste.nativeElement, 'width', '10%');
+    }
+  }
 }
