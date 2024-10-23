6ea78d545b3e181bf831dc9ca2161a56d8a0737d
Dantedevil10 <jonhjooj2907@gmail.com>
2024-10-23 16:18:31 -0300

    ajustes pequenos

--- src/app/pagina/home-log/home-log.component.ts
+++ src/app/pagina/home-log/home-log.component.ts
@@ -1,4 +1,5 @@
-import { Component,HostListener, OnInit } from '@angular/core';
+import { Component, HostListener, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-home-log',
@@ -6,37 +7,40 @@ import { Component,HostListener, OnInit } from '@angular/core';
   styleUrl: './home-log.component.css'
 })
 export class HomeLogComponent {
-
-
-
+  
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
 
   ngOnInit() {
-    this.adjustHeight();
-  }
-
-
-  @HostListener('window:resize', ['$event'])
-  onResize(event: any) {
-    this.adjustHeight();
-  }
-  adjustHeight() { //essa função ajusta o template de acordo com o zoom do navegador
-    const zoomLevel = Math.round(window.devicePixelRatio * 100);
-    const secondDiv = document.getElementById('second');
-
-    if (zoomLevel === 100) {
-      if (secondDiv) {
-        secondDiv.style.height = '102.5%';
-      }
-    } else if (zoomLevel === 90){
-      if (secondDiv) {
-        secondDiv.style.height = '114%';
-      }
-    } else {
-      if (secondDiv) {
-        secondDiv.style.height = '129%';
-      }
+    if (isPlatformBrowser(this.platformId)) {
+      this.adjustHeight();
     }
   }
 
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    if (isPlatformBrowser(this.platformId)) {
+      this.adjustHeight();
+    }
+  }
 
+  adjustHeight() {
+    if (isPlatformBrowser(this.platformId)) {
+      const zoomLevel = Math.round(window.devicePixelRatio * 100);
+      const secondDiv = document.getElementById('second');
+
+      if (zoomLevel === 100) {
+        if (secondDiv) {
+          secondDiv.style.height = '102.5%';
+        }
+      } else if (zoomLevel === 90) {
+        if (secondDiv) {
+          secondDiv.style.height = '114%';
+        }
+      } else {
+        if (secondDiv) {
+          secondDiv.style.height = '129%';
+        }
+      }
+    }
+  }
 }
